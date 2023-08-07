c7e60da50a64bad1a141e605fe43feb1db29c5b4
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-08-07 13:11:44 +0900

    update sql

--- server/database/mysql/sql/2_create_post_table.sql
+++ server/database/mysql/sql/2_create_post_table.sql
@@ -6,7 +6,7 @@ CREATE TABLE `posts` (
   `app_url`     varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `slide_url`   varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `description` text          COLLATE utf8mb4_bin NOT NULL,
-  `created_at`  TIMESTAMP    COLLATE utf8mb4_bin NOT NULL,
+  `created_at`  timestamp    COLLATE utf8mb4_bin NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- server/database/mysql/sql/4_create_comment_table.sql
+++ server/database/mysql/sql/4_create_comment_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `comments` (
   `post_id`     varchar(255) COLLATE utf8mb4_bin NOT NULL,
   `content`     text COLLATE utf8mb4_bin NOT NULL,
   `type`        int(3) NOT NULL DEFAULT '0',
-  `created_at`  datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_at`  timestamp    COLLATE utf8mb4_bin NOT NULL DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
