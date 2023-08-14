d3e6c23be81e4e3ca43065b4b6ae3fa814caac50
39TO <taiyo.murase39@gmail.com>
2023-08-14 15:35:03 +0900

    fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -19,8 +19,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "https://sayka.vercel.app/ogp.png",
-        width: 700,
-        height: 400,
       },
     ],
     type: "article",
