87dd8222f271cdce20d001b8cfe139dddf22b814
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-08-16 10:48:06 +0900

    update: create updated_at on post table

--- server/database/mysql/sql/2_create_post_table.sql
+++ server/database/mysql/sql/2_create_post_table.sql
@@ -9,6 +9,7 @@ CREATE TABLE `posts` (
   `figma_url`   varchar(255) COLLATE utf8mb4_bin,
   `description` text          COLLATE utf8mb4_bin NOT NULL,
   `created_at`  timestamp    COLLATE utf8mb4_bin NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at`  timestamp    COLLATE utf8mb4_bin NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
