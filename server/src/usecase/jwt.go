b69eeeaaf4de2acdd32eefb83fa5de9132e6280e
Masuda-1246 <yaojiezengtian38@gmail.com>
2023-08-18 15:55:48 +0900

    fix: header to cookie

--- server/src/usecase/jwt.go
+++ server/src/usecase/jwt.go
@@ -23,7 +23,7 @@ func NewJwtUsecase(ar repository.AuthRepository) IJwtUsecase {
 }
 
 func (ju *JwtUsecase) GetUserIdFromJwtToken(ctx *gin.Context) (string, error) {
-	tokenString := ctx.Request.Header.Get("jwt")
+	tokenString, err := ctx.Cookie("token")
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
