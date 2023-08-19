dc8e2238dda7e72829311bd0334989f1ebcdd4d2
39TO <taiyo.murase39@gmail.com>
2023-08-19 16:45:52 +0900

    fix

--- client/src/api/auth/user.ts
+++ client/src/api/auth/user.ts
@@ -6,7 +6,4 @@ import { AuthUserResponse } from "./types";
 
 const { serverURL } = getEnv();
 export const getLoggedInUser = async (token: string) =>
-  await apiClient.get<AuthUserResponse>(
-    `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/user`,
-    token
-  );
+  await apiClient.get<AuthUserResponse>(`${serverURL}/auth/user`, token);
