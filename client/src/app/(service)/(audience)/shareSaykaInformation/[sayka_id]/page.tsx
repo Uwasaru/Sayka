e9ce78adcb10e9cdedbfd8557bd8c03619fb5b1c
39TO <taiyo.murase39@gmail.com>
2023-10-30 15:46:33 +0900

    fix

--- client/src/app/(service)/(audience)/shareSaykaInformation/[sayka_id]/page.tsx
+++ client/src/app/(service)/(audience)/shareSaykaInformation/[sayka_id]/page.tsx
@@ -1,37 +1,34 @@
 import { readSayka } from "@/api";
 
 import { Sayka } from "../../_components/Sayka";
-import { Metadata } from "next";
 import { getEnv } from "@/utils/env";
 
 type TProps = {
   params: { sayka_id: string };
 };
 
-const { clientURL } = getEnv();
+// export async function generateMetadata({ params }: TProps): Promise<Metadata> {
+//   const saykaRes = await readSayka(params.sayka_id);
+//   if (saykaRes.type === "error")
+//     throw new Error("データが取得できませんでした。");
+//   const sayka = saykaRes.value.data;
+//   if (!sayka) throw new Error("投稿がありません");
 
-export async function generateMetadata({ params }: TProps): Promise<Metadata> {
-  const saykaRes = await readSayka(params.sayka_id);
-  if (saykaRes.type === "error")
-    throw new Error("データが取得できませんでした。");
-  const sayka = saykaRes.value.data;
-  if (!sayka) throw new Error("投稿がありません");
-
-  return {
-    openGraph: {
-      title: sayka.title,
-      description: sayka.description,
-      url: `${clientURL}/shareSaykaInformation/${sayka.id}}`,
-      siteName: "Sayka",
-      images: [
-        {
-          url: `${clientURL}/og?title=${sayka.title}&userName=${sayka.user.id}`,
-        },
-      ],
-      type: "website",
-    },
-  };
-}
+//   return {
+//     openGraph: {
+//       title: sayka.title,
+//       description: sayka.description,
+//       url: `${clientURL}/shareSaykaInformation/${sayka.id}}`,
+//       siteName: "Sayka",
+//       images: [
+//         {
+//           url: `${clientURL}/og?title=${sayka.title}&userName=${sayka.user.id}`,
+//         },
+//       ],
+//       type: "website",
+//     },
+//   };
+// }
 
 const Page = async ({ params }: TProps) => {
   const saykaRes = await readSayka(params.sayka_id);
